543a165b24d7233cb8079c213d5fc9ef162af6c3
Charlie Fordon <fordoncf@gmail.com>
2022-02-01 15:10:36 -0500

    working move

--- t^3.rb
+++ t^3.rb
@@ -1,48 +1,53 @@
 class Game_Board
-  @@moves = 0
+  attr_accessor :gameboard 
   def initialize
+    @moves = 0
     @gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     @show_board = "#{@gameboard[6]} | #{@gameboard[7]} | #{@gameboard[8]}
 #{@gameboard[3]} | #{@gameboard[4]} | #{@gameboard[5]}
 #{@gameboard[0]} | #{@gameboard[1]} | #{@gameboard[2]}"
   end
 
-  def current_board
+  def move_iter
+    @moves += 1
+  end
+
+  def update_board
+    @show_board = "#{self.gameboard[6]} | #{self.gameboard[7]} | #{self.gameboard[8]}
+#{self.gameboard[3]} | #{self.gameboard[4]} | #{self.gameboard[5]}
+#{self.gameboard[0]} | #{self.gameboard[1]} | #{self.gameboard[2]}"
+  end
+
+  def get_board
     @show_board
   end
 
-  def move(player)
-    @@moves += 1
-    puts "#{player}, make your move."
-    move = gets.strip
-    @gameboard.map! { |x| x == move ? 'Z' : x }
-    return @gameboard
+  def move_number
+    @moves
+  end
+
+  def move(move, id)
+    self.move_iter
+    self.gameboard[move-1] = id
+    self.update_board
+  end
+
+  def run_game
+    puts "Please enter name, Player One."
+    @player_one = gets.strip
+    puts "Thank you, #{@player_one}"
+    puts "Please enter name, Player Two."
+    @player_two = gets.strip
+    puts "Thank you, #{@player_two}"
   end
 end
 
-class Players < Game_Board
-    def initialize
-      puts "Please enter name, Player One."
-      @name_one = gets.strip
-      puts "Thank you, #{@name_one}"
-      puts "Please enter name, Player Two."
-      @name_two = gets.strip
-      puts "Thank you, #{@name_two}"
-    end
 
-    def get_name_one
-      @name_one
-    end
 
-    def get_name_two
-      @name
-    end
-end
 
-players = Players.new
-players.move(players.get_name_one)
-puts player_one.current_board
-
+game = Game_Board.new
+game.move(4, "X")
+puts game.get_board
 
 
 
